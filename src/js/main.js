fdfcde649ce06fb6efb06c0eb9f94db5c168c805
Borys <borys.baca@gmail.com>
2022-01-04 14:29:30 +0100

    Event on buttons

--- src/js/main.js
+++ src/js/main.js
@@ -1,32 +1,41 @@
 const title = document.querySelectorAll('.stat h2')
 const currentTime = document.querySelectorAll('.stat p:nth-of-type(1)')
 const previousTime = document.querySelectorAll('.stat p:nth-of-type(2)')
-const periodOption = 1;
+const btns = document.querySelectorAll('.user__btn')
+let periodOption = 1;
+
+function optionSwitch() {
+    console.log(this)
+    periodOption = Number(this.dataset.id)
+    fetch('data.json')
+        .then(res => res.json())
+        .then(data => appendData(data))
+        .catch(err => console.error(err))
 
 
 
-fetch('data.json')
-    .then(res => res.json())
-    .then(data => appendData(data))
-    .catch(err => console.error(err))
+    function appendData(data) {
+        console.log(data)
+        for (let i = 0; i < data.length; i++) {
+            console.log(data[i].title)
+            title[i].innerHTML = data[i].title
 
-
-
-function appendData(data) {
-    console.log(data)
-    for (let i = 0; i < data.length; i++) {
-        console.log(data[i].title)
-        title[i].innerHTML = data[i].title
-
-        if (periodOption == 1) {
-            currentTime[i].innerHTML = data[i].timeframes.daily.current;
-            previousTime[i].innerHTML = data[i].timeframes.daily.previous;
-        } else if (periodOption == 2) {
-            currentTime[i].innerHTML = data[i].timeframes.weekly.current;
-            previousTime[i].innerHTML = data[i].timeframes.weekly.previous;
-        } else if (periodOption == 3) {
-            currentTime[i].innerHTML = data[i].timeframes.monthly.current;
-            previousTime[i].innerHTML = data[i].timeframes.monthly.previous;
+            if (periodOption == 1) {
+                currentTime[i].innerHTML = data[i].timeframes.daily.current;
+                previousTime[i].innerHTML = data[i].timeframes.daily.previous;
+            } else if (periodOption == 2) {
+                currentTime[i].innerHTML = data[i].timeframes.weekly.current;
+                previousTime[i].innerHTML = data[i].timeframes.weekly.previous;
+            } else if (periodOption == 3) {
+                currentTime[i].innerHTML = data[i].timeframes.monthly.current;
+                previousTime[i].innerHTML = data[i].timeframes.monthly.previous;
+            }
         }
     }
 }
+
+
+
+btns.forEach(item => {
+    item.addEventListener('click', optionSwitch)
+})
