aa5da69724a7ec91dacea28948327ff8a3bf223f
Borys <borys.baca@gmail.com>
2022-01-04 14:09:40 +0100

    js, index changes

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,9 @@
-const work = document.querySelector('.stat--work')
-const play = document.querySelector('.stat--play')
-const title = document.querySelector('.stat--title')
-const study = document.querySelector('.stat--study')
-const exercise = document.querySelector('.stat--exersie')
-const selfcare = document.querySelector('.stat--selfcare')
+const title = document.querySelectorAll('.stat h2')
+const currentTime = document.querySelectorAll('.stat p:nth-of-type(1)')
+const previousTime = document.querySelectorAll('.stat p:nth-of-type(2)')
+const periodOption = 1;
+
+
 
 fetch('data.json')
     .then(res => res.json())
@@ -13,7 +13,20 @@ fetch('data.json')
 
 
 function appendData(data) {
+    console.log(data)
     for (let i = 0; i < data.length; i++) {
         console.log(data[i].title)
+        title[i].innerHTML = data[i].title
+
+        if (periodOption == 1) {
+            currentTime[i].innerHTML = data[i].timeframes.daily.current;
+            previousTime[i].innerHTML = data[i].timeframes.daily.previous;
+        } else if (periodOption == 2) {
+            currentTime[i].innerHTML = data[i].timeframes.weekly.current;
+            previousTime[i].innerHTML = data[i].timeframes.weekly.previous;
+        } else if (periodOption == 3) {
+            currentTime[i].innerHTML = data[i].timeframes.monthly.current;
+            previousTime[i].innerHTML = data[i].timeframes.monthly.previous;
+        }
     }
 }
